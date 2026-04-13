7b5ed77ade1c78dea6c631e1a9012d6f3528612b
Priyank Moradiya <153429763+Priyankm23@users.noreply.github.com>
2026-04-13 19:11:37 +0530

    refactor: simplify PresentationControls configuration in PhoneCanvas component

--- src/canvas/PhoneMockup.tsx
+++ src/canvas/PhoneMockup.tsx
@@ -105,8 +105,7 @@ export default function PhoneCanvas() {
 
         <PresentationControls
           global
-          config={{ mass: 2, tension: 500 }}
-          snap={{ mass: 4, tension: 1500 }}
+          snap
           rotation={[0, 0.3, 0]}
           polar={[-Math.PI / 3, Math.PI / 3]}
           azimuth={[-Math.PI / 1.4, Math.PI / 1.4]}
